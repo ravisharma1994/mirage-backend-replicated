5806c37a694e4f4f30e75229e8820f3aaf11ebe2
Subham Subhasis Patra <test_git_010@outlook.com>
2023-02-26 11:33:31 +0530

    added note

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-
+// TEST API
 
 const URL = "https://replicate.com/api/models/stability-ai/stable-diffusion/versions/f178fa7a1ae43a9a9af01b833b9d2ecf97b1bcb0acfd2dc5dd04895e042863f1/predictions/35smlahezvbf5f4q6rrj2kxk6u";
 
